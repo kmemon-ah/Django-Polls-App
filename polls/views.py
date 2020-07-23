a983ad54a64a04a455250ad021dfe97f4d5918dd
K.M. Emon Ahmed <emon15-1205@diu.edu.bd>
2020-07-23 21:51:07 +0600

    multiple changes

--- polls/views.py
+++ polls/views.py
@@ -3,25 +3,47 @@ from django.shortcuts import render,get_object_or_404
 from .models import Question,Choice
 from django.urls import reverse
 # from django.template import loader
+from django.db.models import F
+from django.urls import reverse
+from django.views import generic
 
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date') [:5]
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list':latest_question_list,
-    }
-    # return HttpResponse (template.render(context,request))
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date') [:5]
+#     # template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list':latest_question_list,
+#     }
+#     # return HttpResponse (template.render(context,request))
+#     return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
+#     # def get_quet(self):
+#     #     question = get_object_or_404(Question,pk=question_id)
+#     #     return render(request,'polls/detail.html', {'question':question})
+
+# class ResultsView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/results.html'
 
 # def detail(request,question_id):
 #     return HttpResponse("You are looking at question %s." %question_id)
 
 def results(request,question_id):
-    response = "You are looking at the result of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
+
+
 def vote (request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     try:
@@ -32,7 +54,8 @@ def vote (request,question_id):
             'error_message':"You didn't select a choice",
         })
     else:
-        selected_choice.votes += 1
+        # selected_choice.votes += 1 --can occur race condition
+        selected_choice.votes= F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
